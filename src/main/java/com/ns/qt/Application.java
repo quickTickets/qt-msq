5eae0b58c6b7383c79887a5cfa8c426273181e6a
Sergey_Samarkin <Sergey_Samarkin@epam.com>
2016-12-07 17:20:50 +0300

    start with docker

--- src/main/java/com/ns/qt/Application.java
+++ src/main/java/com/ns/qt/Application.java
@@ -4,6 +4,28 @@ package com.ns.qt;
  * Created by Sergey_Samarkin on 12/6/2016.
  */
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "WELCOME";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
+/*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,4 +38,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-}
+}*/
